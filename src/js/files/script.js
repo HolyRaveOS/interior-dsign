908f735148cfcc530f88ab4c044932537fee6877
HolyRaveOS <kitlaruk1995@gmail.com>
2023-09-05 09:19:46 +0300

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,5 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+// import { log } from 'gulp-util'
+// import { isMobile } from './functions.js'
+// // Підключення списку активних модулів
+// import { flsModules } from './modules.js'
