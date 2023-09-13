f7ea1dc51eeb1a137c4ff2638069cca701ba15d6
HolyRaveOS <kitlaruk1995@gmail.com>
2023-09-13 09:51:34 +0300

    made animations

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,5 +1,10 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-// import { log } from 'gulp-util'
-// import { isMobile } from './functions.js'
-// // Підключення списку активних модулів
-// import { flsModules } from './modules.js'
+import { isMobile } from './functions.js'
+// Підключення списку активних модулів
+import { flsModules } from './modules.js'
+import 'animate.css'
+var wow = new WOW({
+  boxClass: 'wow',
+  animateClass: 'animate__animated',
+})
+wow.init()
